fb828fc13b4ea432519552c2d5e912bb8aeac0cb
franpham <franpham@gmail.com>
2015-09-02 18:30:12 -1000

    all tests passed

--- vault.js
+++ vault.js
@@ -1,4 +1,13 @@
 'use strict';
 module.exports = function() {
-  
+  var map = {};
+
+  return {
+  	setValue : function (key, value) {
+  		map[key] = value;
+  	},
+  	getValue : function (key) {
+  		return typeof map[key] === 'undefined' ? null : map[key];
+  	}
+  };
 };
